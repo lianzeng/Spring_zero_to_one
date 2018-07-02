2714bbbfaea3b1e14ecadd7721960b341ee3ac35
lianzeng <zenliang@foxmail.com>
2018-07-02 23:35:46 +0800

    add failure test case without product code

---
+++ src/main/java/org/litespring/beans/factory/support/DefaultBeanFactory.java
@@ -0,0 +1,22 @@
+package org.litespring.beans.factory.support;
+
+import org.litespring.beans.BeanDefinition;
+import org.litespring.beans.factory.BeanFactory;
+
+public class DefaultBeanFactory implements BeanFactory {
+
+	public DefaultBeanFactory(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public BeanDefinition getBeanDefinition(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getBean(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
